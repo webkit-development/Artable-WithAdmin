0a83907efdc463fd4dee9962a2200e0d42b68b75
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-04-06 15:06:37 -0400

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Kevin Stradtman on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
